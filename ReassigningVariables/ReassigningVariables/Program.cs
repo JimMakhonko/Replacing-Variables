1fcc99aa527e08fa9f4dfe0d4023d3eb325f5e09
Ievgenii Makhonko <yobaua@gmail.com>
2021-03-23 12:04:05 +0800

    First Commit

---
+++ ReassigningVariables/ReassigningVariables/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ReassigningVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 5;
+            int b = 3;
+
+            Console.WriteLine("First assign a = -a - b.");
+            a = -a - b;
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+
+            Console.WriteLine("Second assign b = -a - b.");
+            b = -a - b;
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+
+            Console.WriteLine("Third assign a = -a - b.");
+            a = -a - b;
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+        }
+    }
+}
